e780abe05556ce6e8a9792936e2a8aeeebe996a0
ChrisR57 <cr_cbr@outlook.com>
2019-10-26 15:51:23 +0100

    Initial commit

---
+++ src/com/learningjavaprogrammingcrashcourse/Main.java
@@ -0,0 +1,51 @@
+package com.learningjavaprogrammingcrashcourse;
+
+import java.text.NumberFormat;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String apples = "Apples";
+        int appleQuantity = 8;
+        double applePrice = 190.50;
+        String oranges = "Oranges";
+        int orangeQuantity = 12;
+        double orangePrice = 80;
+
+        String column1Heading = "Fruit";
+        String column2Heading = "Quantity";
+        String column3Heading = "Price";
+
+// https://dzone.com/articles/java-string-format-examples
+        System.out.printf("%s %s %s%n", column1Heading, column2Heading, column3Heading );
+        System.out.printf("%s %d %.2f pence%n", apples, appleQuantity, applePrice);     // %.2f prints double to 2 decimal places
+        System.out.printf("%s %d %.2f pence%n\n", oranges, orangeQuantity, orangePrice);
+
+
+        System.out.printf("%12s %8s %6s%n", column1Heading, column2Heading, column3Heading ); // %12s prints String column is 12 wide
+        System.out.printf("%12s %8d %6.2f pence%n", apples, appleQuantity, applePrice);     // %6.2f prints double to 2 decimal places column is 6 wide
+        System.out.printf("%12s %8d %6.2f pence%n\n", oranges, orangeQuantity, orangePrice);
+
+        // Left align column "Strings" with negative values
+        System.out.printf("%-12s %8s %6s%n", column1Heading, column2Heading, column3Heading ); // %12s prints String column is 12 wide
+        System.out.printf("%-12s %-8d %6.2f pence%n", apples, appleQuantity, applePrice);     // %6.2f prints double to 2 decimal places column is 6 wide
+        System.out.printf("%-12s %-8d %6.2f pence%n\n", oranges, orangeQuantity, orangePrice);
+
+        // Using currency formatting
+        // normally specify the formatting at the top of the class
+        // have put it here to show it'd needed
+
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+        currencyFormat.setMaximumFractionDigits(3);
+
+        System.out.printf("%-12s %8s %6s%n", column1Heading, column2Heading, column3Heading ); // %12s prints String column is 12 wide
+        System.out.printf("%-12s %-8d %6s %n", apples, appleQuantity,
+                currencyFormat.format(applePrice));     // appends currency sign to the left need to be defined as %s
+        System.out.printf("%-12s %-8d %6s %n\n", oranges, orangeQuantity,
+                currencyFormat.format(orangePrice));     // appends currency sign to the left need to be defined as %s
+
+
+    }
+}
